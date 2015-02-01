81f28d7f3428ae340ea0d1297521567c1bea7b55
Rudi Heinen <rheinen@schubergphilis.com>
2015-02-01 15:58:30 +0100

    bump ver

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'jared.king@biola.edu, rheinen@schubergphilis.com'
 license          'Apache 2.0'
 description      'Installs and configures Gluster servers and clients'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.0.8'
+version          '2.0.9'
 depends          'apt'
 depends          'yum'
